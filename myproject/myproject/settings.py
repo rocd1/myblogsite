99aef7c94cc802e274a039f0eef2049c6e2b562f
roc <d1roc1985@gmail.com>
2024-03-13 21:14:44 +0800

    added vercel config

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''#secretkey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
